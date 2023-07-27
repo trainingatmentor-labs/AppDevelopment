35053c2263aa0dde1cf20aac6a3a944bcb0a8893
trainingatmentor-labs <training@mentor-labs.in>
2023-07-27 12:02:03 +0530

    Main File to Commit

---
+++ TestApp/src/training/iqgateway/AppEntry.java
@@ -0,0 +1,9 @@
+package training.iqgateway;
+
+public class AppEntry {
+
+	public static void main(String[] args) {
+		System.out.println("Main Function ... ");
+		System.out.println("New Value  ... ");
+	}
+}
